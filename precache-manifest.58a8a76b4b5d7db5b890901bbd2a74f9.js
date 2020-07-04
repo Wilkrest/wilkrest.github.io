663fb20a42bf361b3711f523faf53615d8d71cd2
Wilkrest <jarirez@hotmail.com>
2020-07-04 14:41:21 -0500

    Updates

---
+++ precache-manifest.58a8a76b4b5d7db5b890901bbd2a74f9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d85c515db9792f1d8d1d61a1f61543ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a160b5e1800d22d404b2",
+    "url": "/static/css/main.407941dc.chunk.css"
+  },
+  {
+    "revision": "608973a98a1c934a3026",
+    "url": "/static/js/2.51e66746.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.51e66746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a160b5e1800d22d404b2",
+    "url": "/static/js/main.3bc7d516.chunk.js"
+  },
+  {
+    "revision": "537cc0fe65674ec3b4a4",
+    "url": "/static/js/runtime-main.25d8385d.js"
+  },
+  {
+    "revision": "101992b6defb9ce7659f68a7be29676f",
+    "url": "/static/media/cliff.101992b6.JPG"
+  },
+  {
+    "revision": "36be02a6b607c1251e491d4e17d54b60",
+    "url": "/static/media/fingerman.36be02a6.jpg"
+  },
+  {
+    "revision": "cd18fffd46c3cdda77636d1fb77be1d6",
+    "url": "/static/media/nickswen1.cd18fffd.jpg"
+  },
+  {
+    "revision": "eea37720b3883b422f0b32a603c50072",
+    "url": "/static/media/silhouette-of-grass-under-white-sky-925743.eea37720.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wilkrest.github.io/precache-manifest.aa692dc418f81d6e4d549e88ffee6c1d.js"
+  "/precache-manifest.58a8a76b4b5d7db5b890901bbd2a74f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wilkrest.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
